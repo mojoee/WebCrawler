1cc589af9504d8e20c06ab2f483734df775a220d
mojoee <47809126+mojoee@users.noreply.github.com>
2022-02-12 23:46:46 +0800

    created function parse

--- scraper.py
+++ scraper.py
@@ -3,3 +3,8 @@ import scrapy
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2016pip']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+                pass
